f4a1c68e3276b63cb0207bb13d36e623bc6386c2
AASHSIH <96695005+devxashish@users.noreply.github.com>
2026-07-14 23:40:31 +0530

    Add copyright header to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,9 @@
+/**
+ * Copyright © 2025 Ashish. All rights reserved.
+ * This source code is the property of Ashish and may not be reproduced, distributed, 
+ * or transmitted in any form without explicit written permission.
+ */
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
